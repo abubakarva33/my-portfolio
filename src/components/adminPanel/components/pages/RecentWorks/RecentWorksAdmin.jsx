8845946f7a1c7256ace7f9d7f9c29f97088db46f
Abubakar <abubakarsiddikl.info@gmail.com>
2023-08-10 00:38:16 +0600

    project admin

--- src/components/adminPanel/components/pages/RecentWorks/RecentWorksAdmin.jsx
+++ src/components/adminPanel/components/pages/RecentWorks/RecentWorksAdmin.jsx
@@ -1,9 +1,51 @@
+import { useLoaderData } from 'react-router-dom';
 import './RecentWorksAdmin.css';
+import Table from 'react-bootstrap/Table';
+import { AiOutlineEye, AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai'
 
 const RecentWorksAdmin = () => {
+    const tableData = useLoaderData();
     return (
         <div>
-            <h1> recent works admin</h1>
+            <div className=' pageBox'>
+                <div className=' pageBoxInner'>
+                    <div>
+                        <h4>MANAGE RECENT PROJECTS</h4>
+                        <p>All Projects</p>
+                    </div>
+                    <button className='btn btn-primary'> Add Project</button>
+                </div>
+                <Table hover className=''>
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Title</th>
+                            <th>Project Tag</th>
+                            <th>Date Created</th>
+                            <th>Date Edited</th>
+                            <th style={{ width: '135px' }} >Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            tableData.map((data, ind) =>
+                                <tr key={data.id}>
+                                    <td>{ind + 1}</td>
+                                    <td className='colspan-2'>{data.serviceName}</td>
+                                    <td>{data.mainTag}</td>
+                                    <td>{data.registered}</td>
+                                    <td>--</td>
+                                    <td className='d-flex' style={{ width: '135px' }}>
+                                        <h6 className='p-2 me-2 border rounded text-light bg-primary tableActionIcon'><AiOutlineEye /></h6>
+                                        <h6 className='p-2 me-2 border rounded text-light bg-success tableActionIcon'><AiOutlineEdit /></h6>
+                                        <h6 className='p-2 me-2 border rounded text-light bg-secondary tableActionIcon'><AiOutlineDelete /></h6>
+                                    </td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </Table>
+            </div>
         </div>
     );
 };
--- src/components/adminPanel/layouts/LayoutAdmin.jsx
+++ src/components/adminPanel/layouts/LayoutAdmin.jsx
@@ -1,12 +1,34 @@
 import { Outlet } from "react-router-dom";
 import NavbarAdmin from "../components/NavbarAdmin/NavbarAdmin";
+import './LayoutAdmin.css';
+import { Col, Row } from "react-bootstrap";
 
 const LayoutAdmin = () => {
     return (
-        <div>
-            <NavbarAdmin/>
-            <Outlet/>
-        </div>
+        <Row md={2} xs={1} className="admin-layout g-0">
+            <Col xs={12} md={3}>
+                <div className="navbarAdmin">
+                    <h1>Welcome Admin</h1>
+                    <NavbarAdmin />
+                </div>
+            </Col>
+            <Col xs={12} md={9}>
+                <div className="mx-3">
+                    <div className="d-flex align-items-center justify-content-between">
+                        <h3>WELCOME PORTFOLIO ADMIN</h3>
+                        <div className="d-flex  flex-column align-items-end mt-3">
+                            <img src='/Images/userImg2.webp' alt="" className="img-fluid navImg" />
+                            <div>
+                                <p>{`${new Date().getHours()} : ${new Date().getMinutes()} | ${new Date().getDate()}-${new Date().getMonth()}-${new Date().getFullYear()} | Admin Account`}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <Outlet />
+                    </div>
+                </div>
+            </Col>
+        </Row>
     );
 };
 
