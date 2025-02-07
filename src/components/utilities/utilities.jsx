f2e9dea50d5ae1ffd7d961e1f99d465536c0384a
Abubakar <abubakarsiddikl.info@gmail.com>
2025-02-07 22:01:23 +0600

    update

--- src/components/utilities/utilities.jsx
+++ src/components/utilities/utilities.jsx
@@ -13,27 +13,3 @@ export const nullLinkWarning = async ({ title, text }) => {
 export const dynamicSize = (percentage) => {
   return `${(window.innerWidth * 100) / 100 / percentage}px`;
 };
-
-const getNeomorphismTextStyle = ({
-  backgroundColor = "#212428", // Dark background
-  textColor = "var(--color-primary)",
-  fontSize = "30px",
-  fontWeight = "bold",
-}) => {
-  return {
-    display: "inline-block",
-    backgroundColor: backgroundColor,
-    color: textColor, // Slight contrast to make text visible
-    fontSize: fontSize,
-    fontWeight: fontWeight,
-    letterSpacing: "-2px",
-    textShadow: "rgb(17 19 20) 10px 10px 10px, rgb(31 35 39) -10px -10px 10px",
-  };
-};
-
-const NeomorphicWord = ({ word, styleProps }) => {
-  const style = getNeomorphismTextStyle(styleProps);
-  return <span style={style}>{word}</span>;
-};
-
-export default NeomorphicWord;
